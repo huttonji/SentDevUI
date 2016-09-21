79a78cebd2417d804047f169808bc31aca41329e
jamesHutton <james@braingrid.org>
2016-09-20 20:20:34 -0400

    First Commit, simple Serial Terminal

---
+++ Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.tb_output = New System.Windows.Forms.TextBox()
+        Me.tb_input = New System.Windows.Forms.TextBox()
+        Me.button_send = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM69"
+        '
+        'tb_output
+        '
+        Me.tb_output.Location = New System.Drawing.Point(23, 27)
+        Me.tb_output.Name = "tb_output"
+        Me.tb_output.Size = New System.Drawing.Size(450, 20)
+        Me.tb_output.TabIndex = 0
+        '
+        'tb_input
+        '
+        Me.tb_input.Location = New System.Drawing.Point(23, 53)
+        Me.tb_input.Multiline = True
+        Me.tb_input.Name = "tb_input"
+        Me.tb_input.Size = New System.Drawing.Size(450, 478)
+        Me.tb_input.TabIndex = 1
+        '
+        'button_send
+        '
+        Me.button_send.Location = New System.Drawing.Point(506, 133)
+        Me.button_send.Name = "button_send"
+        Me.button_send.Size = New System.Drawing.Size(75, 23)
+        Me.button_send.TabIndex = 2
+        Me.button_send.Text = "send"
+        Me.button_send.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1417, 911)
+        Me.Controls.Add(Me.button_send)
+        Me.Controls.Add(Me.tb_input)
+        Me.Controls.Add(Me.tb_output)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents tb_output As TextBox
+    Friend WithEvents tb_input As TextBox
+    Friend WithEvents button_send As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,60 @@
+﻿Imports System
+Imports System.Threading
+Imports System.IO.Ports
+Imports System.ComponentModel
+
+
+
+Public Class Form1
+
+    Dim availablePorts As Array
+
+
+    'Dim portButton As New System.Windows.Forms.Button()  'adds a new instance of the class "button" (like a prefix)
+
+
+    Delegate Sub setTextCallBack(ByVal [text] As String)  'a delegate points to a function (unknown before runtime)
+
+
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        SerialPort1.Open()
+
+
+
+    End Sub
+
+
+
+
+
+    'Incoming Serial handling///////////////////////////////////////////////////
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        ReceivedText(SerialPort1.ReadExisting)
+    End Sub
+
+    Private Sub ReceivedText(ByVal [text] As String)
+        If Me.tb_input.InvokeRequired Then
+            Dim x As New setTextCallBack(AddressOf ReceivedText)
+            Me.Invoke(x, New Object() {(text)})
+        Else
+            Me.tb_input.Text &= [text]
+        End If
+    End Sub
+
+    Private Sub button_send_Click(sender As Object, e As EventArgs) Handles button_send.Click
+        SerialPort1.WriteLine(tb_output.Text)
+    End Sub
+
+    Private Sub tb_output_KeyDown(sender As Object, e As KeyEventArgs) Handles tb_output.KeyDown
+        If e.KeyData = Keys.Enter Then
+            SerialPort1.WriteLine(tb_output.Text)
+            tb_output.Text = ""
+        End If
+    End Sub
+End Class
